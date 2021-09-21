d53d65b2e2648e84de3e239620e20e282891a2d5
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-09-21 16:17:35 +0100

    commit

--- ficha2/Form2.vb
+++ ficha2/Form2.vb
@@ -1,3 +1,11 @@
 ﻿Public Class Form2
+    Private Sub lbl_rotulo_Click(sender As Object, e As EventArgs) Handles lbl_rotulo.Click
+        lbl_rotulo.Text = ""
+        txt_texto1.Text = ""
+    End Sub
 
+    Private Sub btn_repor_Click(sender As Object, e As EventArgs) Handles btn_repor.Click
+        lbl_rotulo.Text = "isto é um rótulo"
+        txt_texto1.Text = "isto é uma caixa de texto"
+    End Sub
 End Class
