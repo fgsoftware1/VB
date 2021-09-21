d32c951a1258193e30767b9836dc8d27c31b706e
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-09-21 15:10:17 +0100

    commit

--- ficha2/Form1.Designer.vb
+++ ficha2/Form1.Designer.vb
@@ -22,10 +22,74 @@ Partial Class Form1
     'Não o modifique usando o editor de códigos.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.lbl_Nome = New System.Windows.Forms.Label()
+        Me.txt_Nome = New System.Windows.Forms.TextBox()
+        Me.btn_mostra = New System.Windows.Forms.Button()
+        Me.txt_resultado = New System.Windows.Forms.TextBox()
+        Me.lbl_resultado = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lbl_Nome
+        '
+        Me.lbl_Nome.AutoSize = True
+        Me.lbl_Nome.Location = New System.Drawing.Point(60, 68)
+        Me.lbl_Nome.Name = "lbl_Nome"
+        Me.lbl_Nome.Size = New System.Drawing.Size(79, 13)
+        Me.lbl_Nome.TabIndex = 0
+        Me.lbl_Nome.Text = "Nome do aluno"
+        '
+        'txt_Nome
+        '
+        Me.txt_Nome.Location = New System.Drawing.Point(145, 65)
+        Me.txt_Nome.Name = "txt_Nome"
+        Me.txt_Nome.Size = New System.Drawing.Size(100, 20)
+        Me.txt_Nome.TabIndex = 1
+        '
+        'btn_mostra
+        '
+        Me.btn_mostra.Location = New System.Drawing.Point(145, 91)
+        Me.btn_mostra.Name = "btn_mostra"
+        Me.btn_mostra.Size = New System.Drawing.Size(100, 23)
+        Me.btn_mostra.TabIndex = 2
+        Me.btn_mostra.Text = "mostra mensgaem"
+        Me.btn_mostra.UseVisualStyleBackColor = True
+        '
+        'txt_resultado
+        '
+        Me.txt_resultado.Location = New System.Drawing.Point(145, 120)
+        Me.txt_resultado.Name = "txt_resultado"
+        Me.txt_resultado.Size = New System.Drawing.Size(100, 20)
+        Me.txt_resultado.TabIndex = 4
+        '
+        'lbl_resultado
+        '
+        Me.lbl_resultado.AutoSize = True
+        Me.lbl_resultado.Location = New System.Drawing.Point(89, 120)
+        Me.lbl_resultado.Name = "lbl_resultado"
+        Me.lbl_resultado.Size = New System.Drawing.Size(50, 13)
+        Me.lbl_resultado.TabIndex = 3
+        Me.lbl_resultado.Text = "resultado"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(509, 304)
+        Me.Controls.Add(Me.txt_resultado)
+        Me.Controls.Add(Me.lbl_resultado)
+        Me.Controls.Add(Me.btn_mostra)
+        Me.Controls.Add(Me.txt_Nome)
+        Me.Controls.Add(Me.lbl_Nome)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents lbl_Nome As Label
+    Friend WithEvents txt_Nome As TextBox
+    Friend WithEvents btn_mostra As Button
+    Friend WithEvents txt_resultado As TextBox
+    Friend WithEvents lbl_resultado As Label
 End Class
--- ficha2/Form1.vb
+++ ficha2/Form1.vb
@@ -1,3 +1,5 @@
 ﻿Public Class Form1
+    Private Sub btn_mostra_Click(sender As Object, e As EventArgs) Handles btn_mostra.Click
 
+    End Sub
 End Class
