96942819b9a3b4f4acdab17c5d1df980cbd7a1a4
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-09-21 16:17:35 +0100

    commit

--- ficha2/Form2.vb
+++ ficha2/Form2.vb
@@ -1,4 +1,4 @@
-﻿Public Class Form2
+Public Class Form2
     Private Sub btn_apagar_Click_1(sender As Object, e As EventArgs) Handles btn_apagar.Click
         lbl_rotulo.Text = ""
         txt_texto1.Text = ""
@@ -7,5 +7,15 @@
     Private Sub btn_repor_Click(sender As Object, e As EventArgs) Handles btn_repor.Click
         lbl_rotulo.Text = "isto é um rótulo"
         txt_texto1.Text = "isto é uma caixa de texto"
+    End Sub
+
+    Private Sub btn_corFundo_Click(sender As Object, e As EventArgs) Handles btn_corFundo.Click
+        lbl_rotulo.BackColor = Color.Red
+        txt_texto1.ForeColor = Color.Purple
+    End Sub
+
+    Private Sub btn_corTexto_Click(sender As Object, e As EventArgs) Handles btn_corTexto.Click
+        lbl_rotulo.ForeColor = Color.Blue
+        txt_texto1.ForeColor = Color.GreenYellow
     End Sub
 End Class
