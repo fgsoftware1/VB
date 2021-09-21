22c41196f4cb3f1c8a254847367a17ea03c40e61
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-09-21 15:24:49 +0100

    commit

--- ficha2/Form1.vb
+++ ficha2/Form1.vb
@@ -1,5 +1,8 @@
 ﻿Public Class Form1
     Private Sub btn_mostra_Click(sender As Object, e As EventArgs) Handles btn_mostra.Click
+        'declaraçao e inicializaçao de uma variavel
+        Dim valor1 As Integer = 0
 
+        txt_resultado.Text = "Bem vindo, " & txt_Nome.Text
     End Sub
 End Class
