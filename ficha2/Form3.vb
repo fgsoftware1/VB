a02b214b41abf6d96c38511723dee9e11e350261
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-09-27 10:21:35 +0100

    commit

--- ficha2/Form3.vb
+++ ficha2/Form3.vb
@@ -1,3 +1,15 @@
 ﻿Public Class Form3
+    Private Sub btn_calcular_Click(sender As Object, e As EventArgs) Handles btn_calcular.Click
+        Dim comprimento As Integer, largura
+        Dim area As Integer, perimetro
 
+        comprimento = Convert.ToInt32(txt_comprimento.Text)
+        largura = Convert.ToInt32(txt_largura.Text)
+
+        area = comprimento * largura
+        perimetro = 2 * comprimento + 2 * largura
+
+        txt_resultado.Text = "A área é igual a: " & area
+        txt_resultado.Text += vbNewLine & "O perimetro é igual a: " & perimetro
+    End Sub
 End Class
