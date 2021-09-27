0211502bdef2bab1fd671241d2453e5db5798d30
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-09-27 12:46:29 +0100

    commit

--- ficha3/Form2.Designer.vb
+++ ficha3/Form2.Designer.vb
@@ -22,9 +22,42 @@ Partial Class Form2
     'Não o modifique usando o editor de códigos.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.lbl_invocar = New System.Windows.Forms.Label()
+        Me.btn_invocar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lbl_invocar
+        '
+        Me.lbl_invocar.AutoSize = True
+        Me.lbl_invocar.Location = New System.Drawing.Point(29, 35)
+        Me.lbl_invocar.Name = "lbl_invocar"
+        Me.lbl_invocar.Size = New System.Drawing.Size(106, 13)
+        Me.lbl_invocar.TabIndex = 0
+        Me.lbl_invocar.Text = "invocar MessageBox"
+        '
+        'btn_invocar
+        '
+        Me.btn_invocar.Location = New System.Drawing.Point(202, 35)
+        Me.btn_invocar.Name = "btn_invocar"
+        Me.btn_invocar.Size = New System.Drawing.Size(75, 23)
+        Me.btn_invocar.TabIndex = 1
+        Me.btn_invocar.Text = "invocar"
+        Me.btn_invocar.UseVisualStyleBackColor = True
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(511, 289)
+        Me.Controls.Add(Me.btn_invocar)
+        Me.Controls.Add(Me.lbl_invocar)
+        Me.Name = "Form2"
         Me.Text = "Form2"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents lbl_invocar As Label
+    Friend WithEvents btn_invocar As Button
 End Class
--- ficha3/Form2.vb
+++ ficha3/Form2.vb
@@ -1,3 +1,13 @@
 ﻿Public Class Form2
+    Private Sub btn_invocar_Click(sender As Object, e As EventArgs) Handles btn_invocar.Click
+        Dim valor As String
 
+        valor = MsgBox("mostrou mensagem", vbQuestion + vbYesNo, "título da janela")
+
+        If valor = vbYes Then
+            End
+        Else
+            MsgBox("entao vamos continuar")
+        End If
+    End Sub
 End Class
