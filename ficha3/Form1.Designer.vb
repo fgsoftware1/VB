34246275989d142e46521174e0298154e92d0668
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-09-27 12:27:18 +0100

    commit

--- ficha3/Form1.Designer.vb
+++ ficha3/Form1.Designer.vb
@@ -22,10 +22,64 @@ Partial Class Form1
     'Não o modifique usando o editor de códigos.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.lbl_invocar = New System.Windows.Forms.Label()
+        Me.btn_invocar = New System.Windows.Forms.Button()
+        Me.lbl_valor = New System.Windows.Forms.Label()
+        Me.txt_numero = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'lbl_invocar
+        '
+        Me.lbl_invocar.AutoSize = True
+        Me.lbl_invocar.Location = New System.Drawing.Point(44, 54)
+        Me.lbl_invocar.Name = "lbl_invocar"
+        Me.lbl_invocar.Size = New System.Drawing.Size(87, 13)
+        Me.lbl_invocar.TabIndex = 0
+        Me.lbl_invocar.Text = "invocar InputBox"
+        '
+        'btn_invocar
+        '
+        Me.btn_invocar.Location = New System.Drawing.Point(235, 54)
+        Me.btn_invocar.Name = "btn_invocar"
+        Me.btn_invocar.Size = New System.Drawing.Size(75, 23)
+        Me.btn_invocar.TabIndex = 1
+        Me.btn_invocar.Text = "invocar"
+        Me.btn_invocar.UseVisualStyleBackColor = True
+        '
+        'lbl_valor
+        '
+        Me.lbl_valor.AutoSize = True
+        Me.lbl_valor.Location = New System.Drawing.Point(47, 106)
+        Me.lbl_valor.Name = "lbl_valor"
+        Me.lbl_valor.Size = New System.Drawing.Size(138, 13)
+        Me.lbl_valor.TabIndex = 2
+        Me.lbl_valor.Text = "Qual foi o valor introduzido?"
+        '
+        'txt_numero
+        '
+        Me.txt_numero.Location = New System.Drawing.Point(235, 106)
+        Me.txt_numero.Name = "txt_numero"
+        Me.txt_numero.Size = New System.Drawing.Size(100, 20)
+        Me.txt_numero.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(429, 185)
+        Me.Controls.Add(Me.txt_numero)
+        Me.Controls.Add(Me.lbl_valor)
+        Me.Controls.Add(Me.btn_invocar)
+        Me.Controls.Add(Me.lbl_invocar)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents lbl_invocar As Label
+    Friend WithEvents btn_invocar As Button
+    Friend WithEvents lbl_valor As Label
+    Friend WithEvents txt_numero As TextBox
 End Class
--- ficha3/Form1.vb
+++ ficha3/Form1.vb
@@ -1,3 +1,7 @@
 ﻿Public Class Form1
+    Private Sub btn_invocar_Click(sender As Object, e As EventArgs) Handles btn_invocar.Click
+        Dim valor As Integer
 
+        valor = InputBox("Introduza um valor inteiro", "Insira algo", 0)
+    End Sub
 End Class
