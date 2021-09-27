9eb98d76ee229ab2a381abf7f03ba469952eb474
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-09-27 10:21:35 +0100

    commit

--- ficha2/My Project/Application.Designer.vb
+++ ficha2/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.ficha2.Form2
+            Me.MainForm = Global.ficha2.Form3
         End Sub
     End Class
 End Namespace
