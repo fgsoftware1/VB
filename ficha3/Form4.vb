e62d3c1c1231c9d63853d3b8eaa4673b1879ba61
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-09-28 15:15:11 +0100

    commit

--- ficha3/Form4.vb
+++ ficha3/Form4.vb
@@ -1,3 +1,10 @@
 ﻿Public Class Form4
+    Private Sub btn_random_Click(sender As Object, e As EventArgs) Handles btn_random.Click
+        Dim num1 As Integer
 
+        Randomize()
+
+        num1 = Convert.ToInt32(Rnd() * 100) + 1
+        MessageBox.Show("numero gerado foi: " & num1)
+    End Sub
 End Class
