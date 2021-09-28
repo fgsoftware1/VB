fbe817d3073eeff2b37c9df7c708dcbdfa5b55d6
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-09-28 15:48:31 +0100

    commit

--- ficha3/Form5.vb
+++ ficha3/Form5.vb
@@ -1,5 +1,7 @@
 ﻿Public Class Form5
     Private Sub btn_mostrar_Click(sender As Object, e As EventArgs) Handles btn_mostrar.Click
         txt_data.Text = DateTime.Now.Date.ToString("dd MM yyyy")
+        txt_hora.Text = DateTime.Now.TimeOfDay.ToString()
+        txt_data_hora.Text = DateTime.Now.ToString
     End Sub
 End Class
