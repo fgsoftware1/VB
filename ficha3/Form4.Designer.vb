b612c3536546221faf70369f45b9d7d30d81d944
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-09-28 15:06:47 +0100

    commit

--- ficha3/Form4.Designer.vb
+++ ficha3/Form4.Designer.vb
@@ -22,16 +22,42 @@ Partial Class Form4
     'Não o modifique usando o editor de códigos.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.lbl_random = New System.Windows.Forms.Label()
+        Me.btn_random = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'lbl_random
+        '
+        Me.lbl_random.AutoSize = True
+        Me.lbl_random.Location = New System.Drawing.Point(39, 31)
+        Me.lbl_random.Name = "lbl_random"
+        Me.lbl_random.Size = New System.Drawing.Size(112, 13)
+        Me.lbl_random.TabIndex = 0
+        Me.lbl_random.Text = "gerar numero aleatorio"
+        '
+        'btn_random
+        '
+        Me.btn_random.Location = New System.Drawing.Point(42, 57)
+        Me.btn_random.Name = "btn_random"
+        Me.btn_random.Size = New System.Drawing.Size(109, 23)
+        Me.btn_random.TabIndex = 1
+        Me.btn_random.Text = "gerar"
+        Me.btn_random.UseVisualStyleBackColor = True
         '
         'Form4
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(434, 295)
+        Me.Controls.Add(Me.btn_random)
+        Me.Controls.Add(Me.lbl_random)
         Me.Name = "Form4"
         Me.Text = "Form4"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents lbl_random As Label
+    Friend WithEvents btn_random As Button
 End Class
