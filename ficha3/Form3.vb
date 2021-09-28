656860b5a50c08060c77f411f43986d19c84ed2b
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-09-28 14:28:50 +0100

    commit

--- ficha3/Form3.vb
+++ ficha3/Form3.vb
@@ -1,2 +1,5 @@
 ﻿Public Class Form3
+    Private Sub btn_calcular_Click(sender As Object, e As EventArgs) Handles btn_calcular.Click
+        txt_sqrt_resultado.Text = Math.Sqrt(Convert.ToDouble(txt_sqrt_num1.Text))
+    End Sub
 End Class
