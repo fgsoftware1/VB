184c5178bd4ed684da7bfe432cc5954391b7341d
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-10-04 12:37:18 +0100

    Update Form1.vb

--- ficha4/Form1.vb
+++ ficha4/Form1.vb
@@ -1,2 +1,7 @@
 ﻿Public Class Form1
+    Private Sub btn_limpar_Click(sender As Object, e As EventArgs) Handles btn_limpar.Click
+        txt_num1.Text = ""
+        txt_num2.Text = ""
+        txt_resultado.Text = ""
+    End Sub
 End Class
