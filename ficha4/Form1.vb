ccd85df2c1d9efa6d69101f7ce322d05fa05e1c7
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-10-04 12:47:54 +0100

    Update Form1.vb

--- ficha4/Form1.vb
+++ ficha4/Form1.vb
@@ -11,6 +11,21 @@
     End Sub
 
     Private Sub btn_calcular_Click(sender As Object, e As EventArgs) Handles btn_calcular.Click
+        Dim valor1 As Integer = 0, valor2 = 0
 
+        valor1 = Val(txt_num1.Text)
+        valor2 = Val(txt_num2.Text)
+
+        If radio_somar.Checked = True Then
+            txt_resultado.Text = valor1 + valor2
+        ElseIf radio_subtrair.Checked = True Then
+            txt_resultado.Text = valor1 - valor2
+        ElseIf radio_multiplicar.Checked = True Then
+            txt_resultado.Text = valor1 * valor2
+        ElseIf radio_dividir.Checked = True Then
+            txt_resultado.Text = valor1 / valor2
+        Else
+            MsgBox("nenhuma operaçao selecionada")
+        End If
     End Sub
 End Class
