93419839dc1ad5f0417073bbeb5c294b7496c6d8
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-10-11 11:35:58 +0100

    commit

--- ficha4/Form2.vb
+++ ficha4/Form2.vb
@@ -1,3 +1,10 @@
 ﻿Public Class Form2
-
+    Private Sub btn_limpar_Click(sender As Object, e As EventArgs) Handles btn_limpar.Click
+        txt_unidades.Text = ""
+        txt_preço.Text = ""
+        radio_taxa10.Checked = False
+        radio_taxa15.Checked = False
+        check_desconto5.Checked = False
+        txt_resultado.Text = ""
+    End Sub
 End Class
