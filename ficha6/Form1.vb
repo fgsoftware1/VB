3f46521c2124037bfb724c425f7fe62192d96e21
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-10-18 11:43:27 +0100

    commit

--- ficha6/Form1.vb
+++ ficha6/Form1.vb
@@ -1,12 +1,36 @@
 ﻿Public Class btn_calcular
+    Dim dados(3) As Integer, valor = 0
+
     Private Sub btn_limpar_Click(sender As Object, e As EventArgs) Handles btn_limpar.Click
         txt_soma.Text = ""
         txt_media.Text = ""
         txt_maximo.Text = ""
         txt_minimo.Text = ""
+        ListBox1.Items.Clear()
     End Sub
 
     Private Sub btn_terminar_Click(sender As Object, e As EventArgs) Handles btn_terminar.Click
         End
+    End Sub
+
+    Private Sub btn_introduzir_Click(sender As Object, e As EventArgs) Handles btn_introduzir.Click
+
+        For i = 0 To 3
+            valor = Convert.ToInt32(InputBox("Introduza os valores"))
+
+            dados(i) = valor
+
+            ListBox1.Items.Add(valor)
+        Next
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim soma As Integer
+        Dim maximo As Integer
+        Dim minimo As Integer
+
+        For i = 0 To 3
+            soma = soma + dados(3)
+        Next
     End Sub
 End Class
