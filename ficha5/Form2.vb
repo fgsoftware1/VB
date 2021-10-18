26d42e7c76055db5c739f5fec77aae5827e91bc5
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-10-18 10:15:21 +0100

    commit

--- ficha5/Form2.vb
+++ ficha5/Form2.vb
@@ -1,3 +1,9 @@
 ﻿Public Class Form2
+    Private Sub btn_calcular_Click(sender As Object, e As EventArgs) Handles btn_calcular.Click
+        Dim resultado As Integer = 0
 
+        resultado = Module_Calculos.SomaDados(num_n1.Value, num_n2.Value)
+
+        txt_resultado.Text = resultado
+    End Sub
 End Class
