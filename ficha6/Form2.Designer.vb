60e6670ec427df8bfc76e2bf26cda61a3f69b0bf
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-10-19 14:30:51 +0100

    commit

---
+++ ficha6/Form2.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.btn_inserir = New System.Windows.Forms.Button()
+        Me.btn_limpar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(181, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 83)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(178, 225)
+        Me.ListBox1.TabIndex = 1
+        '
+        'btn_inserir
+        '
+        Me.btn_inserir.Location = New System.Drawing.Point(220, 83)
+        Me.btn_inserir.Name = "btn_inserir"
+        Me.btn_inserir.Size = New System.Drawing.Size(108, 57)
+        Me.btn_inserir.TabIndex = 2
+        Me.btn_inserir.Text = "inserir"
+        Me.btn_inserir.UseVisualStyleBackColor = True
+        '
+        'btn_limpar
+        '
+        Me.btn_limpar.Location = New System.Drawing.Point(220, 243)
+        Me.btn_limpar.Name = "btn_limpar"
+        Me.btn_limpar.Size = New System.Drawing.Size(108, 65)
+        Me.btn_limpar.TabIndex = 3
+        Me.btn_limpar.Text = "limpar"
+        Me.btn_limpar.UseVisualStyleBackColor = True
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(369, 353)
+        Me.Controls.Add(Me.btn_limpar)
+        Me.Controls.Add(Me.btn_inserir)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form2"
+        Me.Text = "Form2"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents btn_inserir As Button
+    Friend WithEvents btn_limpar As Button
+End Class
