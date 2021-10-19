db8fc18e4dcee14346c2783cc40d2356b3ef315e
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-10-19 14:45:51 +0100

    commit

--- ficha6/Form2.vb
+++ ficha6/Form2.vb
@@ -1,3 +1,20 @@
 ﻿Public Class Form2
+    Private Sub btn_limpar_Click(sender As Object, e As EventArgs) Handles btn_limpar.Click
+        ListBox1.Items.Clear()
+    End Sub
 
+    Private Sub btn_inserir_Click(sender As Object, e As EventArgs) Handles btn_inserir.Click
+        Dim valor As Integer = 1
+        Dim dados(0 To valor) As Integer
+
+        valor = Val(InputBox("Quantos dados pretende inserir?", "quantidade"))
+
+        For i = 0 To valor
+            valor = Convert.ToInt32(InputBox("Introduza os valores"))
+
+            dados(i) = valor
+
+            ListBox1.Items.Add(valor)
+        Next
+    End Sub
 End Class
