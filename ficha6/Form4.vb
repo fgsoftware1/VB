65859b4b86dd264d9f926649a072f9c08a44e3d0
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-10-25 11:21:43 +0100

    commit

--- ficha6/Form4.vb
+++ ficha6/Form4.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Form4
-
+    Private Sub btn_invocar_Click(sender As Object, e As EventArgs) Handles btn_invocar.Click
+        numero1 = num_num1.Value
+        numero2 = num_num2.Value
+    End Sub
 End Class
