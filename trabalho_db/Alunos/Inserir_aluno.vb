8e4529bf8e6dda28f9576b902384217b7a7cc2c7
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-10-26 15:47:22 +0100

    commit

--- trabalho_db/Alunos/Inserir_aluno.vb
+++ trabalho_db/Alunos/Inserir_aluno.vb
@@ -1,13 +1,13 @@
 ﻿Public Class Inserir_aluno
-    Private Sub btn_cancelar_Click(sender As Object, e As EventArgs) Handles btn_cancelar.Click
-
-    End Sub
-
     Private Sub btn_inserir_Click(sender As Object, e As EventArgs) Handles btn_inserir.Click
 
     End Sub
 
     Private Sub pic_imagem_Click(sender As Object, e As EventArgs) Handles pic_imagem.Click
+        Dim openFileDialog1 As OpenFileDialog
+    End Sub
+
+    Private Sub btn_cancelar_Click(sender As Object, e As EventArgs) Handles btn_cancelar.Click
 
     End Sub
 End Class
