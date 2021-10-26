adccc50a09294d3a6aa8a22c5d461c8f5a696a21
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-10-26 15:54:51 +0100

    commit

--- trabalho_db/Alunos/Inserir_aluno.vb
+++ trabalho_db/Alunos/Inserir_aluno.vb
@@ -1,10 +1,22 @@
 ﻿Public Class Inserir_aluno
+    Dim imageUpload As Image
+
     Private Sub btn_inserir_Click(sender As Object, e As EventArgs) Handles btn_inserir.Click
 
     End Sub
 
     Private Sub pic_imagem_Click(sender As Object, e As EventArgs) Handles pic_imagem.Click
         Dim openFileDialog1 As OpenFileDialog
+
+        With openFileDialog1
+            .CheckFileExists = True
+            .ShowReadOnly = False
+            .Filter = "All Files|*.*|Bitmap Files (*)|*.bmp; *.gif; *.jpg"
+
+            If .ShowDialog = DialogResult.OK Then
+                pic_imagem.Image = Image.FromFile(.FileName)
+            End If
+        End With
     End Sub
 
     Private Sub btn_cancelar_Click(sender As Object, e As EventArgs) Handles btn_cancelar.Click
