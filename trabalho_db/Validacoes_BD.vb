0817c47a89658fed9c8403b40a95b8169cd115f0
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-10-26 16:27:25 +0100

    commit

--- trabalho_db/Validacoes_BD.vb
+++ trabalho_db/Validacoes_BD.vb
@@ -1,3 +1,10 @@
 ﻿Module Validacoes_BD
-    Public Function
+    Public Function Valida_Numeros(valor As Object, nome_campo As String)
+        If IsNumeric(valor) = False Then
+            MsgBox("campo" & "nome_campo" & "nao é numérico", vbCritical + vbOKOnly,
+                   "erro de validaçao numérica")
+
+            Valida_Numeros = -1
+        End If
+    End Function
 End Module
