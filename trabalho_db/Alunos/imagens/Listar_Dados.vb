f22e34a65339f2b627b53f913dda93d2ecc66590
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-11-08 12:22:09 +0000

    commit

--- trabalho_db/Alunos/imagens/Listar_Dados.vb
+++ trabalho_db/Alunos/imagens/Listar_Dados.vb
@@ -1,3 +1,14 @@
-﻿Public Class Listar_Dados
+﻿Imports MySql.Data.MySqlClient
 
+Public Class Listar_Dados
+    Private Sub btn_carregar_Click(sender As Object, e As EventArgs) Handles btn_carregar.Click
+        Dim result As MySqlDataAdapter
+        Dim table As New DataTable()
+
+        result = Module_BD.Executar_Sql_DataGridView("SELECT * FROM alunos")
+
+        result.Fill(table)
+
+        DataGridView1.DataSource = table
+    End Sub
 End Class
