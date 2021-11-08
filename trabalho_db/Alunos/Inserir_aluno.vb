03fbe6485aa37eec33e1f1cd5cf6c8b271814bb3
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-11-08 10:54:50 +0000

    commit

--- trabalho_db/Alunos/Inserir_aluno.vb
+++ trabalho_db/Alunos/Inserir_aluno.vb
@@ -1,8 +1,23 @@
-﻿Public Class Inserir_aluno
+﻿Imports System.IO
+
+Public Class Inserir_aluno
     Dim imageUpload As Image
     Dim imageFilename As String
 
     Private Sub btn_inserir_Click(sender As Object, e As EventArgs) Handles btn_inserir.Click
+        Dim ativo = IIf(cmb_ativo.Text = "Sim", 1, 0)
+        Dim nome_ficheiro = System.IO.Path.GetFileName(imageFilename)
+        Dim extensao_ficheiro = System.IO.Path.GetExtension(imageFilename)
+        Dim src = AppDomain.CurrentDomain.BaseDirectory
+        Dim result = Module_BD.Executar_Sql_Command("INSERT INTO " &
+                                                    "alunos (Nome, Morada, Data_Nasc, Genero, Contato, Imagem, Ativo" &
+                                                    "VALUES ('" & txt_nome.Text & "', '" & txt_morada.Text & "', " &
+                                                    "'" & txt_data.Text & "', '" & cmb_genero.Text & "', " &
+                                                    "" & txt_contato.Text & ", '" & nome_ficheiro & "', " &
+                                                    "" & ativo & ")")
+
+        src = Directory.GetParent(src).Parent.Parent.FullName & "/imagens"
+
         If Validacoes_BD.Valida_Numeros(txt_contato.Text, "Contato") = -1 Then
             txt_contato.Text = ""
             Exit Sub
@@ -13,9 +28,22 @@
             Exit Sub
         End If
 
-        Dim ativo = IIf(cmb_ativo.Text = "Sim", 1, 0)
-        Dim nome_ficheiro = System.IO.Path.GetFileName(imageFilename)
-        Dim extensao_ficheiro = System.IO.Path.GetExtension
+        If File.Exists(src & nome_ficheiro) Then
+            MsgBox("já existe a imagem")
+            pic_imagem.Image = Nothing
+            Exit Sub
+        Else
+            File.Copy(imageFilename, src & nome_ficheiro)
+        End If
+
+        Try
+            If IsNothing(result) Then
+                File.Delete(src & nome_ficheiro)
+                MsgBox("Erro na inserçao dos dados")
+            End If
+        Catch ex As MySql.Data.MySqlClient.MySqlException
+            MessageBox.Show(ex.Message)
+        End Try
     End Sub
 
     Private Sub pic_imagem_Click(sender As Object, e As EventArgs) Handles pic_imagem.Click
