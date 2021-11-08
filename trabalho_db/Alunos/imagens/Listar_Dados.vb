bb6f1a4467fe47b22b31109bd2873d980cf17db3
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-11-08 12:41:00 +0000

    commit

--- trabalho_db/Alunos/imagens/Listar_Dados.vb
+++ trabalho_db/Alunos/imagens/Listar_Dados.vb
@@ -1,4 +1,5 @@
-﻿Imports MySql.Data.MySqlClient
+﻿Imports System.IO
+Imports MySql.Data.MySqlClient
 
 Public Class Listar_Dados
     Private Sub btn_carregar_Click(sender As Object, e As EventArgs) Handles btn_carregar.Click
@@ -10,5 +11,18 @@ Public Class Listar_Dados
         result.Fill(table)
 
         DataGridView1.DataSource = table
+    End Sub
+
+    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        Dim valor As Integer
+        Dim src = AppDomain.CurrentDomain.BaseDirectory
+
+        src = Directory.GetParent(src).Parent.Parent.FullName & "\imagens\"
+        valor = DataGridView1.CurrentRow.Index
+
+        txt_id.Text = DataGridView1.Item("ID_Aluno", valor).Value
+        txt_nome = DataGridView1.Item("Nome", valor).Value
+        txt_morada = DataGridView1.Item("Morada", valor).Value
+        pic_imagem.ImageLocation = src & DataGridView1.Item("Imagem", valor).Value
     End Sub
 End Class
--- trabalho_db/My Project/Application.Designer.vb
+++ trabalho_db/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.trabalho_db.Inserir_aluno
+            Me.MainForm = Global.trabalho_db.Listar_Dados
         End Sub
     End Class
 End Namespace
