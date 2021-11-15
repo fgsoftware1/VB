51bceefb49e7a1e4a8c709ec634bdafd761366bd
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-11-15 12:00:13 +0000

    commit

--- trabalho_db/Sumarios/Inserir_Sumario.Designer.vb
+++ trabalho_db/Sumarios/Inserir_Sumario.Designer.vb
@@ -22,9 +22,95 @@ Partial Class Inserir_Sumario
     'Não o modifique usando o editor de códigos.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.lbl_title = New System.Windows.Forms.Label()
+        Me.txt_title = New System.Windows.Forms.TextBox()
+        Me.txt_summary = New System.Windows.Forms.TextBox()
+        Me.txt_Data = New System.Windows.Forms.TextBox()
+        Me.cmb_disciplina = New System.Windows.Forms.ComboBox()
+        Me.cmb_ativo = New System.Windows.Forms.ComboBox()
+        Me.btn_Inserir = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lbl_title
+        '
+        Me.lbl_title.AutoSize = True
+        Me.lbl_title.Location = New System.Drawing.Point(385, 25)
+        Me.lbl_title.Name = "lbl_title"
+        Me.lbl_title.Size = New System.Drawing.Size(73, 13)
+        Me.lbl_title.TabIndex = 0
+        Me.lbl_title.Text = "inserir sumário"
+        '
+        'txt_title
+        '
+        Me.txt_title.Location = New System.Drawing.Point(12, 56)
+        Me.txt_title.Name = "txt_title"
+        Me.txt_title.Size = New System.Drawing.Size(527, 20)
+        Me.txt_title.TabIndex = 1
+        '
+        'txt_summary
+        '
+        Me.txt_summary.Location = New System.Drawing.Point(12, 83)
+        Me.txt_summary.Multiline = True
+        Me.txt_summary.Name = "txt_summary"
+        Me.txt_summary.Size = New System.Drawing.Size(527, 355)
+        Me.txt_summary.TabIndex = 2
+        '
+        'txt_Data
+        '
+        Me.txt_Data.Location = New System.Drawing.Point(545, 56)
+        Me.txt_Data.Name = "txt_Data"
+        Me.txt_Data.Size = New System.Drawing.Size(207, 20)
+        Me.txt_Data.TabIndex = 3
+        '
+        'cmb_disciplina
+        '
+        Me.cmb_disciplina.FormattingEnabled = True
+        Me.cmb_disciplina.Location = New System.Drawing.Point(546, 128)
+        Me.cmb_disciplina.Name = "cmb_disciplina"
+        Me.cmb_disciplina.Size = New System.Drawing.Size(206, 21)
+        Me.cmb_disciplina.TabIndex = 4
+        '
+        'cmb_ativo
+        '
+        Me.cmb_ativo.FormattingEnabled = True
+        Me.cmb_ativo.Location = New System.Drawing.Point(546, 82)
+        Me.cmb_ativo.Name = "cmb_ativo"
+        Me.cmb_ativo.Size = New System.Drawing.Size(206, 21)
+        Me.cmb_ativo.TabIndex = 5
+        '
+        'btn_Inserir
+        '
+        Me.btn_Inserir.Location = New System.Drawing.Point(559, 407)
+        Me.btn_Inserir.Name = "btn_Inserir"
+        Me.btn_Inserir.Size = New System.Drawing.Size(229, 30)
+        Me.btn_Inserir.TabIndex = 6
+        Me.btn_Inserir.Text = "inserir"
+        Me.btn_Inserir.UseVisualStyleBackColor = True
+        '
+        'Inserir_Sumario
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btn_Inserir)
+        Me.Controls.Add(Me.cmb_ativo)
+        Me.Controls.Add(Me.cmb_disciplina)
+        Me.Controls.Add(Me.txt_Data)
+        Me.Controls.Add(Me.txt_summary)
+        Me.Controls.Add(Me.txt_title)
+        Me.Controls.Add(Me.lbl_title)
+        Me.Name = "Inserir_Sumario"
         Me.Text = "Inserir_Sumario"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents lbl_title As Label
+    Friend WithEvents txt_title As TextBox
+    Friend WithEvents txt_summary As TextBox
+    Friend WithEvents txt_Data As TextBox
+    Friend WithEvents cmb_disciplina As ComboBox
+    Friend WithEvents cmb_ativo As ComboBox
+    Friend WithEvents btn_Inserir As Button
 End Class
--- trabalho_db/Sumarios/Inserir_Sumario.vb
+++ trabalho_db/Sumarios/Inserir_Sumario.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Inserir_Sumario
-
+    Private Sub btn_Inserir_Click(sender As Object, e As EventArgs) Handles btn_Inserir.Click
+        Dim ativo = IIf(cmb_ativo.Text = "Sim", 1, 0)
+        Dim disciplina
+    End Sub
 End Class
