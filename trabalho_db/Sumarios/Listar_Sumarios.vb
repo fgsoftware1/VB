8ddfa407c01f1b8760bc588e4a73bebc9cbf494b
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2021-11-15 11:29:54 +0000

    commit

--- trabalho_db/Sumarios/Listar_Sumarios.vb
+++ trabalho_db/Sumarios/Listar_Sumarios.vb
@@ -1,14 +1,25 @@
 ﻿Imports MySql.Data.MySqlClient
 
 Public Class Listar_Sumarios
-    Dim result As MySqlDataAdapter
-    Dim table As New DataTable()
+    Private Sub Listar_Sumarios_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim result As MySqlDataAdapter
+        Dim table As New DataTable()
 
-    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
         result = Module_BD.Executar_Sql_DataGridView("SELECT * FROM sumarios")
 
         result.Fill(table)
 
         DataGridView1.DataSource = table
+    End Sub
+
+    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        Dim valor As Integer
+        Dim src = AppDomain.CurrentDomain.BaseDirectory
+
+        valor = DataGridView1.CurrentRow.Index
+
+        txt_id.Text = DataGridView1.Item("ID_Aluno", valor).Value
+        txt_nome.Text = DataGridView1.Item("Nome", valor).Value
+        txt_morada.Text = DataGridView1.Item("Morada", valor).Value
     End Sub
 End Class
